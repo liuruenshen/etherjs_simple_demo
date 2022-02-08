5ae56c27501fd67eb353d9068565002669da5b78
kiwi <kiwi71728@icloud.com>
2022-02-08 16:38:59 +0800

    refactor: move app related files into components/app folder

---
+++ src/layout/MainLayout.tsx
@@ -0,0 +1,45 @@
+import React, { PropsWithChildren } from "react";
+import Box from "@mui/material/Box";
+
+export function MainLayout({ children }: PropsWithChildren<unknown>) {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "center",
+        width: "100vw",
+        height: "100vh",
+        minHeight: "100vh",
+        backgroundColor: "background.default",
+      }}
+    >
+      <Box
+        sx={(theme) => ({
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "stretch",
+          justifyContent: "flex-start",
+          backgroundColor: theme.palette.background.paper,
+          borderRadius: "10px",
+          overflow: "hidden",
+          [theme.breakpoints.down("sm")]: {
+            width: "95%",
+            height: "95%",
+          },
+          [theme.breakpoints.up("sm")]: {
+            width: "80%",
+            height: "80%",
+          },
+          [theme.breakpoints.up("lg")]: {
+            width: "60%",
+            height: "60%",
+          },
+        })}
+      >
+        {children}
+      </Box>
+    </Box>
+  );
+}
